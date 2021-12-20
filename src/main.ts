5d44c0b5872cff803488c66d008cff29d8d6a480
Lucas Zluhan <lszluhan@hotmail.com>
2021-12-19 21:42:12 -0300

    att

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 
 let app = express();
 app.use(express.json());
-app.use(cors());
 app.use(express.static(__dirname + `/../public/`));
 
 const port = process.env.PORT || 8888;
